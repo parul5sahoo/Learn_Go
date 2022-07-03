4a99218adfa42f7ee26308a8134591973c7b8491
Parul Sahoo <parulsahoo5jan@gmail.com>
2022-07-03 20:40:10 +0530

    added Doc packages

---
+++ exercises/Jedi_level-12/dog/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+type canine struct {
+	name string
+	age uint
+}
+
+func main() {
+	fido := canine {
+		name: "Fido",
+		age: dog.Years(10),
+	}
+	fmt.Println(fido)
+}
+
---
+++ exercises/Jedi_level-12/main.go
@@ -0,0 +1,7 @@
+// Package dog provides functionality to understand dogs better
+package dog
+
+// Years converts human years to dog years
+func Years(n int) int {
+	return n * 10
+}
